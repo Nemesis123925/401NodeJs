1c8f92a4fa72e5714e1c31073c54a672bca3975e
Yuxuan Guo <yguo3044@usc.edu>
2022-01-27 18:38:39 -0800

    include admin schema of mysql in src/resource app.js and mysql.js now is working, can exchange data with the database now

--- src/Mappers/mysql.js
+++ src/Mappers/mysql.js
@@ -1,17 +1,54 @@
-let mysql = require('mysql');
+let config = require("./mysqlConfig")
 
-let connection = mysql.createConnection({
-    host    : 'localhost',
-    user    : 'root',
-    password: 'root',
-    database: '401teamresolve'
-})
+let connection = config.connection
 
-connection.connect();
+let query = "SELECT password FROM adminusers WHERE username = '"
+let insert = "INSERT INTO adminusers(username,password) VALUE('"
+let sql = ""
 
-let query = "SELECT * FROM adminusers"
+function addUser(res, username, password){
+    sql = insert + username + "', '" + password + "')"
+    connection.query(sql, function(error, results, fields){
+        if (error && error.code === 'ER_DUP_ENTRY') {
+            console.log("Duplicated")
+            res.end("Duplicated")
+        }else{
+            console.log(results);
+            res.end("success")
+        }
+    })
+}
+
+function checkUser(res, username, password){
+    sql = query + username +  "'"
+    connection.query(sql, function(error, results, fields){
+        console.log(results)
+        if (error) {
+            throw error
+            /*console.log("Duplicated")
+            res.end("Duplicated")*/
+        }else if(!results.length) {
+            res.end("fail, wrong username")
+        }else{
+            results = JSON.parse(JSON.stringify(results))
+            if(results[0].password === password){
+                res.end("success")
+            }else{
+                res.end("failed, wrong password")
+            }
+            //res.end("success")
+        }
+    })
+}
+/*
 
 connection.query(query, function (error, results, fields) {
     if (error) throw error;
     console.log('The solution is: ', results);
 });
+
+addUser("abc", "12334444")
+*/
+
+exports.addUser = addUser;
+exports.checkUser = checkUser;
--- src/ServerWithExpress/app.js
+++ src/ServerWithExpress/app.js
@@ -1,18 +1,37 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = express.Router();
+const app = express();
+const mysql = require("../Mappers/mysql") // import mysql.js file in Mapper package
 
-app.get('/',(req,res) =>{
-    res.send("Hello World!")
+// indicates body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// reply simply "good" to get
+router.get('/',(req, res) => {
+    res.send("good");
 });
 
-app.get('/hi', (req, res) => {
-    res.send("hi")
+// signup functionality, call the addUser function in mysql.js.
+router.post("/signup",(req, res) => {
+    let user_name = req.body.username;
+    let password = req.body.password;
+    //console.log("User name = " + user_name + ", password = " + password);
+    mysql.addUser(res, user_name, password) // note the pass the response to mysql.js, to let it do the rest
 });
 
-/*app.post('/addUser', (req, res) =>{
-
-})*/
-
-app.listen(8889,() => {
-    console.log("listening on port 8889")
+router.post("/login",(req, res) => {
+    let user_name = req.body.username;
+    let password = req.body.password;
+    console.log("User name = " + user_name + ", password = " + password);
+    mysql.checkUser(res, user_name, password) // note the pass the response to mysql.js, to let it do the rest
 });
+
+// bind app with router
+app.use("/", router);
+// start listening
+app.listen(3000,() => {
+    console.log("Started on PORT 3000");
+})
+
